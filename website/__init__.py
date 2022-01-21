df675937b07ed6b41aefd00e4d4e269a6b2002f3
Jackie Dong <jackie.dong@yale.edu>
2022-01-21 18:09:06 -0500

    More testing

--- website/__init__.py
+++ website/__init__.py
@@ -22,6 +22,9 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
     db.init_app(app)
 
+    with app.app_context():
+        db.create_all()
+
     from .views import views
     from .auth import auth
 
